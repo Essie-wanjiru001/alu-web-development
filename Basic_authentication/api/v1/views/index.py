82776ea8244201b13156dc916bf4836c60c6df40
Essie-wanjiru001 <esthernjeriwanjiru287@gmail.com>
2024-06-01 12:37:15 +0300

    updates

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -4,12 +4,15 @@ Index module for the API
 """
 from flask import Blueprint, jsonify, abort
 
+
 app_views = Blueprint('app_views', __name__)
+
 
 @app_views.route('/api/v1/status', methods=['GET'], strict_slashes=False)
 def status():
     """ Status of the API """
     return jsonify({"status": "OK"}), 200
+
 
 @app_views.route('/api/v1/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized():
